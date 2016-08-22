1a5651ac1b24902f96138c27077d6f62954f1e11
Luis Padron <luispadronedu@gmail.com>
2016-08-22 16:01:40 -0400

    create home page

--- lpadron/lpadron/urls.py
+++ lpadron/lpadron/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+# Import views
+from . import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home)
 ]
