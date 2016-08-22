1d19579f873b7f82d2965c0ac3b56f2194feed06
Luis Padron <luispadronedu@gmail.com>
2016-08-22 17:14:02 -0400

    add first page for articles

--- lpadron/articles/views.py
+++ lpadron/articles/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Article
+
+def article_list(request):
+    return HttpResponse('Articles homepage')
