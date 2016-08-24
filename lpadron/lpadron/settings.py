1d83c0e781f7b8d3357d9cc16c1d882bde1c59ae
Luis Padron <luispadronedu@gmail.com>
2016-08-24 00:05:18 -0400

    add static files

--- lpadron/lpadron/settings.py
+++ lpadron/lpadron/settings.py
@@ -109,3 +109,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Dirs for static files
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
--- lpadron/lpadron/urls.py
+++ lpadron/lpadron/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Import views
 from . import views
@@ -11,3 +12,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
