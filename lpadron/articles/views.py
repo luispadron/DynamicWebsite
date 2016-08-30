c84dd01c873aef831736ab3cef45623c4682097c
Luis Padron <luispadronedu@gmail.com>
2016-08-30 17:12:23 -0400

    remove useless print from article_detail

--- lpadron/articles/views.py
+++ lpadron/articles/views.py
@@ -9,6 +9,5 @@ def article_list(request):
     return render(request, 'articles/article_list.html', {'articles': articles})
 
 def article_detail(request, slug):
-    print(slug)
     article = get_object_or_404(Article, slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
