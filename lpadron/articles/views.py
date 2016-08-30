aed3dd1bf5016ddc79ef44d008d08bd21e4df90d
Luis Padron <luispadronedu@gmail.com>
2016-08-29 21:19:47 -0400

    add detail view

--- lpadron/articles/views.py
+++ lpadron/articles/views.py
@@ -6,3 +6,7 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all()
     return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    return render(request, 'articles/article_detail.html', {'article': article})
