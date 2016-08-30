1c255775eccbc812c6e2d5f35c62828a35847794
Luis Padron <luispadronedu@gmail.com>
2016-08-29 21:28:37 -0400

    add subtitle field to articles model, make date field manual

--- lpadron/articles/models.py
+++ lpadron/articles/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=False)
     title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=140)
     body = models.TextField()
 
     def __str__(self):
