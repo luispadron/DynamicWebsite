1ea3f99aaf928ace08f4302ff6b13e01e158f8c7
Luis Padron <luispadronedu@gmail.com>
2016-08-30 23:30:24 -0400

    removed weird root folder

--- lpadron/settings.py
+++ lpadron/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'lpadron.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['lpadron/templates',],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
