0b2920f67a72409c6a86c5d8db1a5a8d857de49a
Luis Padron <luispadronedu@gmail.com>
2016-09-01 21:58:37 -0400

    add google analytics

--- lpadron/settings.py
+++ lpadron/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r__b4va1(m7mrcal+@rhk@po6q9$$o*0rg*xu&xute2vyyp$v$'
+SECRET_KEY = local_settings.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -136,3 +136,7 @@ STATICFILES_DIRS = (
 # Disqus API Keys
 DISQUS_API_KEY = local_settings.disqus_key
 DISQUS_WEBSITE_SHORTNAME = local_settings.disqus_short_name
+
+# Google analytics
+GOOGLE_ANALYTICS_PROPERTY_ID = 'UA-83584415-1'
+GOOGLE_ANALYTICS_DOMAIN = 'luispadron.com'
